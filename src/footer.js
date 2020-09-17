fd81646e4936d7e2344d820af2e253ebd2eec36a
stephen1705 <stephenask22@gmail.com>
2020-09-17 06:50:19 +0000

    Changes on Design

--- src/footer.js
+++ src/footer.js
@@ -1,55 +1,56 @@
 import React from 'react';
 import './App.css';
+import { Link } from "react-router-dom";
 //import logo from "./logo.svg";
 
-function Footer () {
-  return(
-   <footer className="">
-    <div className="footer_top">
-          back to top
+function Footer() {
+  return (
+    <footer className="">
+      <div className="footer_top">
+        <Link to="">back to top</Link>
+      </div>
+      <ul className="footer_contents">
+        <li>
+          <h4> Get know about us</h4>
+          <ul className="footer_nav">
+            <li>About Us</li>
+            <li>Careers</li>
+            <li>Press Releases</li>
+            <li>Amazon Cares</li>
+            <li>Gift a Smile</li>
+          </ul>
+        </li>
+        <li><h4> Connect with Us</h4>
+          <ul className="footer_nav">
+            <li>About Us</li>
+            <li>Careers</li>
+            <li>Press Releases</li>
+            <li>Amazon Cares</li>
+            <li>Gift a Smile</li>
+          </ul></li>
+        <li><h4> Make Money with Us</h4>
+          <ul className="footer_nav">
+            <li>About Us</li>
+            <li>Careers</li>
+            <li>Press Releases</li>
+            <li>Amazon Cares</li>
+            <li>Gift a Smile</li>
+          </ul></li>
+        <li><h4> Let Us Help You</h4>
+          <ul className="footer_nav">
+            <li>About Us</li>
+            <li>Careers</li>
+            <li>Press Releases</li>
+            <li>Amazon Cares</li>
+            <li>Gift a Smile</li>
+          </ul></li>
+      </ul>
+      <div className="copy_rights">
+        @copyrights reserved
     </div>
-    <ul className="footer_contents">
-      <li>
-        <h4> Get know about us</h4>
-        <ul className="footer_nav">
-          <li>About Us</li>
-          <li>Careers</li>
-          <li>Press Releases</li>
-          <li>Amazon Cares</li>
-          <li>Gift a Smile</li>
-        </ul>
-      </li>
-      <li><h4> Connect with Us</h4>
-      <ul className="footer_nav">
-          <li>About Us</li>
-          <li>Careers</li>
-          <li>Press Releases</li>
-          <li>Amazon Cares</li>
-          <li>Gift a Smile</li>
-        </ul></li>
-      <li><h4> Make Money with Us</h4>
-      <ul className="footer_nav">
-          <li>About Us</li>
-          <li>Careers</li>
-          <li>Press Releases</li>
-          <li>Amazon Cares</li>
-          <li>Gift a Smile</li>
-        </ul></li>
-      <li><h4> Let Us Help You</h4>
-      <ul className="footer_nav">
-          <li>About Us</li>
-          <li>Careers</li>
-          <li>Press Releases</li>
-          <li>Amazon Cares</li>
-          <li>Gift a Smile</li>
-        </ul></li>
-    </ul>
-    <div className="copy_rights">
-      @copyrights reserved
-    </div>
-   </footer>
-   );
-  
+    </footer>
+  );
+
 }
 
 export default Footer;
